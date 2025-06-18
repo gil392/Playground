cf46d9a234c0e54683024d5011dfbc0fe09f0f69
Gil <gilgil.abramovich@gmail.com>
2025-06-18 20:29:27 +0300

    Try fixing that it always takes commit of first/last test. Instead check for the exact same test.

--- server/bisect_all_tests_in_suite_detailed.sh
+++ server/bisect_all_tests_in_suite_detailed.sh
@@ -55,7 +55,12 @@ for test_name in "${FAILED_TEST_NAMES[@]}"; do
 #!/bin/bash
 echo "Running test: $test_name"
 npx jest --detectOpenHandles --forceExit --runInBand "$SUITE_FILE" -t "$test_name"
-exit \$?
+node -e '
+  const data = require("./jest-bisect-result.json");
+  const results = data.testResults.flatMap(tr => tr.assertionResults);
+  const failed = results.find(t => t.title === "$test_name" && t.status === "failed");
+  process.exit(failed ? 1 : 0);
+'
 EOF
 
   chmod +x "$RUNNER_SCRIPT"
