09d1e3640fe0ab84917b78c511779cc5b7032f62
Gil <gilgil.abramovich@gmail.com>
2025-06-19 20:53:52 +0300

    empty commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -37,7 +37,7 @@ app.get("/divide", ((req: Request, res: Response) => {
 
 app.get("/multiply", ((req: Request, res: Response) => {
   const a = Number(req.query.a);
-  const b = Number(req.query.b);
+  const b = Number(req.query.b); //
 
   if (isNaN(a) || isNaN(b)) {
     return res.status(400).send("Invalid numbers");
