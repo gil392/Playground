e56b6bbcf89d82cb8cbb414d70efb889afc8fe66
Gil <gilgil.abramovich@gmail.com>
2025-06-21 04:06:52 +0300

    Empty - can start from here

--- bigProject/apps/backend/src/index.ts
+++ bigProject/apps/backend/src/index.ts
@@ -29,6 +29,7 @@ app.get('/divide', ((req: Request, res: Response) => {
     return res.status(400).send('Invalid numbers');
   }
 
+  //
   // if (b === 0) {
   //   //
   //   return res.status(400).send('Division by zero');
