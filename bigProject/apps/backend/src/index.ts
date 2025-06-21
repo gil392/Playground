4b26d3abbe700b26b59c536180eafa3bc323ae1d
Gil <gilgil.abramovich@gmail.com>
2025-06-21 03:50:13 +0300

    Broke division test

--- bigProject/apps/backend/src/index.ts
+++ bigProject/apps/backend/src/index.ts
@@ -33,7 +33,7 @@ app.get('/divide', ((req: Request, res: Response) => {
     return res.status(400).send('Division by zero');
   }
 
-  res.send({ result: a / b });
+  res.send({ result: a - b });
 }) as RequestHandler);
 
 app.get('/multiply', ((req: Request, res: Response) => {
