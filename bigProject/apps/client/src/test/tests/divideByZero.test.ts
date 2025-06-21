8862bdf5f662ef4cc2af81bf811f41081b6287a7
Gil <gilgil.abramovich@gmail.com>
2025-06-21 04:02:52 +0300

    Empty

--- bigProject/apps/client/src/test/tests/divideByZero.test.ts
+++ bigProject/apps/client/src/test/tests/divideByZero.test.ts
@@ -32,6 +32,7 @@ async function run() {
       throw new Error('No result found: ' + text);
 
     if (text !== 'Division by zero') {
+      //
       console.warn(
         '❌ Test failed: result is supposed to be Division by zero, instead ' +
           text
